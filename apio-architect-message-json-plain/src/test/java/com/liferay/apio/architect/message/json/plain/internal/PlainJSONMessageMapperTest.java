649c9c4b57945ec5c044b7eb74393c4900a9f6d6
Alejandro Hernández <info@alejandrohdezma.com>
2018-04-03 13:08:05 +0200

    APIO-167 Test plain JSON mappers using the new utility

---
+++ apio-architect-message-json-plain/src/test/java/com/liferay/apio/architect/message/json/plain/internal/PlainJSONMessageMapperTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.architect.message.json.plain.internal;
+
+import com.liferay.apio.architect.test.util.json.MessageMapperTesterBuilder;
+
+import javax.ws.rs.core.HttpHeaders;
+
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Javier Gamarra
+ * @author Alejandro Hernández
+ */
+public class PlainJSONMessageMapperTest {
+
+	@Test
+	public void testPlainJSONMessageMappers() {
+		MessageMapperTesterBuilder.httpHeaders(
+			Mockito.mock(HttpHeaders.class)
+		).mediaType(
+			"application/json"
+		).validatePageMessageMapper(
+			new PlainJSONPageMessageMapper<>()
+		).validateSingleModelMessageMapper(
+			new PlainJSONSingleModelMessageMapper<>()
+		);
+	}
+
+}
